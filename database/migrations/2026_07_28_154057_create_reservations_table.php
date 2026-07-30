d5f1242ec811b8c0c4fdb7d568b9479c22b834f5
pavmardev <pavelm725@gmail.com>
2026-07-30 12:12:05 +0200

    creating database schema using laravel migrations

---
+++ database/migrations/2026_07_28_154057_create_reservations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('court_id')->constrained('courts')->onDelete('cascade');
+            $table->foreignId('time_slot_id')->constrained('time_slots')->onDelete('cascade');
+            $table->date('reservation_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
