0837c31d93ef6a52b7a3b06c165c66126f5e488b
pavmardev <pavelm725@gmail.com>
2026-08-10 17:34:16 +0200

    registring testing data in DatabaseSeeder for fullfilment of migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            TimeSlotSeeder::class,
+            MembershipSeeder::class,
+            UserSeeder::class,
+            EquipmentSeeder::class,
+            CourtSeeder::class,
+            FeatureSeeder::class,
+            ReservationSeeder::class,
+            EquipmentReservationSeeder::class,
         ]);
     }
 }
